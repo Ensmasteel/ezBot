8cddecd4f852d6a3a23c31c0eab3faac1b3e5b87
alan le run <alan.lerun@gmail.com>
2024-01-17 15:29:48 +0100

    added a global view camera

--- src/ezbot_static_camera/launch/launch_sim.launch.py
+++ src/ezbot_static_camera/launch/launch_sim.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='ezbot_descr_simul' #<--- CHANGE ME
+    package_name='ezbot_static_camera' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -29,11 +29,6 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    rsp2 = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ezbot_static_camera'),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
-    )
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -50,15 +45,6 @@ def generate_launch_description():
                                    '-z', '1.0' ],
                         output='screen')
 
-    spawn_entity2 = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        namespace='camera_namespace',
-                        name='Pascameracontroller',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'camera',
-                                   '-x','1.0',
-                                   '-y','-1.0',
-                                   '-z', '1.0' ],
-                        output='screen')
     #joystick = IncludeLaunchDescription(
     #            PythonLaunchDescriptionSource([os.path.join(
     #                   get_package_share_directory(package_name),'launch','joystick.launch.py'
@@ -73,8 +59,6 @@ def generate_launch_description():
             default_value=[os.path.join(get_package_share_directory("ezbot_gazebo"), 'worlds', 'table2024Poteaux.world'), ''],
             description='SDF world file'),
         rsp,
-        spawn_entity,
-        rsp2,
-        spawn_entity2,
-        gazebo
+        gazebo,
+        spawn_entity
     ])
