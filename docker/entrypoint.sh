adc0875d27dcd79539a2dad19bada7b19e474834
Vincent Belpois <vincent.belpois@gmail.com>
2024-04-21 23:41:52 +0200

    Be able to launch docker on raspberry's boot (not used)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,11 @@ set -e
 
 id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} --uid ${GID:=1000} ros
 
-source /opt/ros/${ROS_DISTRO}/setup.bash
+source /opt/ros/humble/setup.bash
 source /colcon_ws/install/setup.bash
+
+echo "source /home/ezbot/ezbot/install/setup.bash"
+source /ezbot/ezbot/install/setup.bash
+
 
 exec "$@"
