bd342ea6bf2277d1b3980968241f0f8a926ea3bd
Vincent Belpois <vincent.belpois@gmail.com>
2024-05-09 16:17:05 +0200

    Revert "trying to englobe everytinh under the /lidar namespace"
    This reverts commit a484c72ef5a5d6c9765b3bdb8992fad34c0495cb.

--- src/ldrobot-lidar-ros2/ldlidar_node/launch/ldlidar_with_mgr.launch.py
+++ src/ldrobot-lidar-ros2/ldlidar_node/launch/ldlidar_with_mgr.launch.py
@@ -50,7 +50,6 @@ def generate_launch_description():
         package='nav2_lifecycle_manager',
         executable='lifecycle_manager',
         name='lidar_lifecycle_manager',
-        namespace='lidar',
         output='screen',
         parameters=[
             # YAML files
