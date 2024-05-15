031dc6b062873216b5a24b73b20211cd02c94657
Vincent Belpois <vincent.belpois@gmail.com>
2024-05-15 17:53:06 +0200

    Nav2 kind of works with slam IRL

--- src/ezbot_simulation/launch/launch_sim.launch.py
+++ src/ezbot_simulation/launch/launch_sim.launch.py
@@ -74,7 +74,8 @@ def generate_launch_description():
         DeclareLaunchArgument(
             'world',
             #default_value=[os.path.join('ezbot_simulation','worlds','table2024.world'), ''],
-            default_value=[os.path.join(get_package_share_directory("ezbot_simulation"), 'worlds', 'table_with_panneaux.world'), ''],
+            #default_value=[os.path.join(get_package_share_directory("ezbot_simulation"), 'worlds', 'table_with_panneaux.world'), ''],
+            default_value=[os.path.join(get_package_share_directory("ezbot_simulation"), 'worlds', 'table2024Poteaux.world'), ''],
             description='SDF world file'),
         rsp,
         spawn_entity,
