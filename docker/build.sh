51794164221d7a40cc36a9e7694db0a0df898e7d
Vincent Belpois <vincent.belpois@gmail.com>
2025-01-29 15:04:47 +0100

    removed compilation of arm docker image, and change scripts

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# build a multi-arch image
-docker build --platform linux/amd64 -t vincida/ezbot:testing-x86 -f ./dockerfile-x86 . --push
-docker build --platform linux/arm64 -t vincida/ezbot:testing-arm -f ./dockerfile-arm64 . --push
+#change working directory to the directory of the script
+cd "$(dirname "$0")"
 
+# build a multi-arch image
+#docker build --platform linux/amd64 -t vincida/ezbot:testing-x86 -f ./dockerfile-x86 . --push
+#docker build --platform linux/arm64 -t vincida/ezbot:testing-arm -f ./dockerfile-arm64 . --push
+
+docker build --platform linux/amd64 -t ezbot:testing-x86 -f ./dockerfile-x86 .
+#docker build --platform linux/arm64 -t ezbot:testing-arm -f ./dockerfile-arm64 .
